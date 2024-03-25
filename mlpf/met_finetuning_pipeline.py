545949f12498dd09d1cd3e2f2ae42c31f160e3dc
Farouk <farouk.mokhtar@gmail.com>
2024-03-25 16:19:34 +0100

    up

--- mlpf/met_finetuning_pipeline.py
+++ mlpf/met_finetuning_pipeline.py
@@ -185,7 +185,6 @@ def main():
             trainable=config["model"]["trainable"],
             dtype=dtype,
             lr_schedule=lr_schedule,
-            use_ray=False,
             checkpoint_freq=config["checkpoint_freq"],
             val_freq=config["val_freq"],
         )
@@ -197,5 +196,7 @@ def main():
 if __name__ == "__main__":
 
     # e.g.
-    # noqa: python mlpf/met_finetuning_pipeline.py --dataset clic --data-dir tensorflow_datasets --config parameters/pytorch/pyg-clic.yaml --gpus 1 --prefix MLPF_test1_ --num-epochs 10 --train --load /pfvol/experiments/MLPF_clic_A100_1gpu_pyg-clic_20240322_233518_004447 --gpu-batch-multiplier 5
+    # noqa: python mlpf/met_finetuning_pipeline.py --dataset clic --data-dir tensorflow_datasets --config parameters/pytorch/pyg-clic.yaml --gpus 1 --prefix MLPF_test1_ --num-epochs 10 --train --load /pfvol/experiments/MLPF_clic_A100_1gpu_pyg-clic_20240322_233518_004447/best_weights.pth --gpu-batch-multiplier 5
+    # git checkout 8d9065cba1af49b97c63c4701789a7f7a1fbcd47 /home/jovyan/particleflow/mlpf/pyg/mlpf.py
+    # git checkout 8d9065cba1af49b97c63c4701789a7f7a1fbcd47 /home/jovyan/particleflow/mlpf/pyg/utils.py
     main()
