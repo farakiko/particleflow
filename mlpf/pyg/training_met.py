f76adb9ef2e468b8d9eebe651f59a38dae036cb3
Farouk <farouk.mokhtar@gmail.com>
2024-04-09 18:20:28 +0200

    give MLPFcands + Nulls

--- mlpf/pyg/training_met.py
+++ mlpf/pyg/training_met.py
@@ -117,8 +117,9 @@ def train_and_valid(
             )
 
         else:  # use the MLPF cands
-            p4_masked = ymlpf["momentum"] * msk_ymlpf.unsqueeze(-1)
-            X = torch.cat([p4_masked, ymlpf["cls_id_onehot"], ymlpf["charge"]], axis=-1)
+            # p4_masked = ymlpf["momentum"] * msk_ymlpf.unsqueeze(-1)
+            # X = torch.cat([p4_masked, ymlpf["cls_id_onehot"], ymlpf["charge"]], axis=-1)
+            X = torch.cat([ymlpf["momentum"], ymlpf["cls_id_onehot"], ymlpf["charge"]], axis=-1)
 
         assert X.requires_grad is False, "The MLPF model must be frozen."
 
